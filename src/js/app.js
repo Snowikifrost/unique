7d3c1515f85590008c203b73a02388adea3c4701
Naitra <snowikifrost@gmail.com>
2019-03-19 14:50:44 +0300

    added one section

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,17 @@
-(function($){
-    $(document).ready(function(){
+(function ($) {
+    $(document).ready(function () {
 
-//Липкое меню
-        $(document).on('scroll resize',function(){
+        //Липкое меню
+        let FixedMenu = function () {
             let scrollig = $(document).scrollTop();
-            
-            if(scrollig >= 750){
+            if (scrollig >= 750) {
                 $('nav').addClass('fixed');
             } else {
                 $('nav').removeClass('fixed');
             }
-        })
-
+        }
+        FixedMenu();
+        $(document).on('scroll resize', FixedMenu);
 
 
     });
